2d04850dc70cabbc1fdd705c0cb530c282623680
wonsang.choi <wonsang.choi@e4net.net>
2025-10-19 20:27:51 +0900

    feat(app): 문자열 입력 기능 추가
    콘솔을 통해 "덧셈할 문자열을 입력해 주세요." 안내 문구를 출력하고
    사용자로부터 문자열을 입력받는 기능을 추가함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            run();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static void run() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        // 데이터 입력 받음
+        final String input = readLine();
     }
 }
